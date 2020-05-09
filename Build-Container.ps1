8eb80a6462db018e9753ca568997a088c9bbaa0a
ninckblokje <jeroen.ninckblok@gmail.com>
2020-05-09 12:48:23 +0200

    Cleaned entire project for new structure

--- Build-Container.ps1
+++
@@ -1,3 +0,0 @@
-.\mvnw.cmd clean package
-docker build -t ninckblokje/ping-pong:latest .
-docker push ninckblokje/ping-pong:latest
--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM openliberty/open-liberty:webProfile8
-ADD target/ping-pong.war /config/dropins/
